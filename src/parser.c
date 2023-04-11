00416abd289cfd865e66e66f310d2f6d01d8665e
alejopm03 <perdomomilar@gmail.com>
2023-04-11 11:49:32 -0300

    Add documentation of some functions and macros

--- src/parser.c
+++ src/parser.c
@@ -1,11 +1,36 @@
+/**
+ * @file parser.c
+ *
+ * @brief Implementation of the parser module for tree-based parsing
+ *
+ * This file contains the implementation of the parser module, which is used for tree-based parsing of data.
+ * It includes functions for parsing one level of the tree, handling buffer data and node matching, and
+ * traversing through the tree structure. It also includes logging functions for debugging and troubleshooting
+ * purposes.
+*/
+
+
 #include "parser.h"
 
 /**
  * @brief Parse one level of the tree receiving the buffer and the node that it will match
  *
- * @param buffer the buffer containing the data
- * @param nodes nodes that the buffer will match
- * @return const nodes_t* the next level of the tree
+ * This function parses one level of a tree structure by matching the data in the buffer with the nodes
+ * specified by the `nodes` argument. The `buffer` parameter contains the data to be parsed, and the `nodes`
+ * parameter specifies the nodes that the buffer data will be matched against.
+ *
+ * @param buffer Pointer to the buffer containing the data to be parsed
+ * @param nodes Pointer to the nodes that the buffer data will be matched against
+ *
+ * @return const nodes_t* Pointer to the next level of the tree after parsing the buffer data
+ *
+ * @note This function assumes that the `nodes` argument is a valid pointer to a valid nodes_t structure,
+ *       and that the `buffer` argument is a valid pointer to a valid buffer_t structure.
+ * @note This function may return NULL if an error occurs or if the actual node is a leaf node.
+ * @note The caller of this function should handle the NULL node that represent an end of tree or an error
+ * @note This function may log information, warning, or error messages using logging functions such as LOG_INFO,
+ *       LOG_WARN, and LOG_ERROR. It is assumed that these logging functions are properly defined and configured
+ *       in the system to provide meaningful logs for debugging and troubleshooting purposes.
  */
 const nodes_t *node_parser(buffer_t *buffer, const nodes_t *nodes);
 
--- src/parser.h
+++ src/parser.h
@@ -16,14 +16,10 @@ typedef enum parser_consumer_data
  */
 typedef struct buffer_t
 {
-    /* Points to the begin of the buffer*/
-    const void *begin;
-    /* Points to the to consume buffer*/
-    void *actual;
-    /* Points to the end of the buffer*/
-    const void *end;
-    /* Buffer consumer function */
-    parser_consumer_data_t (*consumer)(struct buffer_t *buffer);
+    const void *begin;  /**< Points to the begin of the buffer */
+    void *actual;       /**< Points to the to consume buffer */
+    const void *end;    /**< Points to the end of the buffer */
+    parser_consumer_data_t (*consumer)(struct buffer_t *buffer);   /**< Buffer consumer function */
 } buffer_t;
 
 typedef enum parser_match
@@ -35,20 +31,17 @@ typedef enum parser_match
 
 typedef struct node_t
 {
-    /* Value pointer */
-    const void *value;
-    /* Handler function */
-    void (*callback)(buffer_t *buffer, const void *value);
-    /* Next node of the tree*/
-    const struct nodes_t *next;
+    const void *value;   /**< Value pointer */
+    void (*callback)(buffer_t *buffer, const void *value); /**< Handler function */
+    const struct nodes_t *next;  /**< Next node of the tree */
 } node_t;
 
 typedef struct nodes_t
 {
-    const char *name;
-    const int size;
-    parser_match_t (*match_func)(void *data, const void *match_value);
-    const node_t *nodes;
+    const char *name;   /**< Node group name */
+    const int size;     /**< size of the array of nodes */
+    parser_match_t (*match_func)(void *data, const void *match_value); /**< Match function */
+    const node_t *nodes;    /**< Nodes array */
 } nodes_t;
 
 /**
@@ -76,7 +69,11 @@ typedef struct nodes_t
 #endif
 
 /**
- * Helper macro for make a array of nodes
+ * @brief Helper macro for making an array of nodes
+ *
+ * @param NAME Name of the array
+ * @param MATCH_FUNC Match function for the nodes
+ * @param ... Array of node_t
  */
 #define MAKE_NODES(NAME, MATCH_FUNC, ...)             \
     const node_t NAME##_NODE[] =                      \
@@ -89,19 +86,22 @@ typedef struct nodes_t
     }
 
 /**
- * Helper macro for make a empty array of nodes this is a workaround 
- * to create circular nodes.
+ * @brief Helper macro for making an empty array of nodes
+ * This is a workaround to create circular nodes.
  * To create circular nodes, first create an empty node before all nodes that have dependency
  * with this node, and after those nodes use the macro FILL_EMPTY_NODES to create the nodes
+ *
  * Example:
- * 
+ * \code
  * MAKE_EMPTY_NODES(foo, &func);
  * MAKE_NODES(bar, &func, {MAKE_NODE("bar", NULL, &foo)});
- * FILL_EMPTY_NODES(foo, {MAKE_NODE("foo", NULL, &bar)})
- * 
- * in this example the next node of foo is bar and the next node of bar is foo
- * 
- *  
+ * FILL_EMPTY_NODES(foo, {MAKE_NODE("foo", NULL, &bar)});
+ * \endcode
+ *
+ * In this example, the next node of foo is bar and the next node of bar is foo
+ *
+ * @param NAME Name of the empty node
+ * @param MATCH_FUNC Match function for the node
  */
 #define MAKE_EMPTY_NODES(NAME, MATCH_FUNC) \
     nodes_t NAME = {                       \
@@ -110,21 +110,23 @@ typedef struct nodes_t
     }
 
 /**
- * Helper macro to fill a empty node
- * NAME: nodes_t name
- * ...: array of node_t
+ * @brief Helper macro to fill an empty node
+ *
+ * @param NAME Name of the node
+ * @param ... Array of node_t
  */
 #define FILL_EMPTY_NODES(NAME, ...)         \
     const node_t NAME##_NODE[] =            \
         __VA_ARGS__;                        \
     NAME.nodes = NAME##_NODE; \
-    *(int *)&NAME.size = sizeof(NAME##_NODE) / sizeof(node_t);
+    *(int*)&NAME.size = sizeof(NAME##_NODE) / sizeof(node_t);
 
 /**
- * Helper macro for crate parsing nodes
- * VALUE is the match case that it be compared with the buffer
- * CALLBACK is the callback function
- * NEXT is the next node
+ * @brief Helper macro for creating parsing nodes
+ *
+ * @param VALUE Value to be matched with the buffer
+ * @param CALLBACK Callback function for the node
+ * @param NEXT Next node in the tree
  */
 #define MAKE_NODE(VALUE, CALLBACK, NEXT) \
     {                                    \
@@ -133,12 +135,20 @@ typedef struct nodes_t
         .next = NEXT,                    \
     }
 
+
 /**
- * Helper macro for crate a wildcard parsing node,
- * is the same of the MAKE_NODE macro but it will match with everything
- * NOTE: nodes are checked sequentially, all nodes after this node will be ignored
- * CALLBACK: is the callback function
- * NEXT: is the next node
+ * @brief Helper macro for creating a wildcard parsing node.
+ *
+ * This macro creates a node that matches with any value in the input buffer.
+ * Nodes are checked sequentially, and all nodes after this wildcard node will be ignored.
+ *
+ * @param CALLBACK A function pointer to the callback function associated with the wildcard node.
+ * @param NEXT     A pointer to the next node in the tree after the wildcard node.
+ *
+ * @note The callback function will be invoked when a wildcard match is found during parsing.
+ * @remark The next node in the tree should be carefully chosen to ensure correct parsing behavior.
+ *         Nodes after this wildcard node will be ignored, so the wildcard node should be placed
+ *         appropriately in the tree.
  */
 #define MAKE_WILDCARD_NODE(CALLBACK, NEXT) \
     {                                      \
@@ -148,9 +158,27 @@ typedef struct nodes_t
     }
 
 /**
- * @brief parser function, receives the buffer and the tree description and parse the buffer
- * @param buffer: buffer to parse
- * @param root_node tree description 
+ * @brief Parse data from buffer using a tree structure starting from the root node
+ *
+ * This function parses data from the buffer using a tree structure, starting from the specified root node.
+ * The buffer data is consumed using a function specified in the `buffer` parameter, and the tree nodes
+ * are traversed based on the consumed data until the end of the tree is reached or an error occurs.
+ *
+ * @param buffer Pointer to the buffer containing the data to be parsed
+ * @param root_node Pointer to the root node of the tree to be traversed
+ *
+ * @note This function assumes that the `buffer` argument is a valid pointer to a valid buffer_t structure,
+ *       and that the `root_node` argument is a valid pointer to a valid nodes_t structure representing the root
+ *       node of the tree.
+ * @note The `buffer` parameter should contain a function pointer for consuming data from the buffer. The return
+ *       value of the function should indicate the status of the consumption, such as OK for successful consumption
+ *       and an appropriate error code for any errors that may occur.
+ * @note The tree structure is expected to be properly initialized with valid nodes and match functions that
+ *       correspond to the expected data format in the buffer. The caller of this function should ensure that
+ *       the tree structure is correctly set up before calling this function to avoid unexpected behavior.
+ * @note This function may log information, warning, or error messages using logging functions such as LOG_INFO,
+ *       LOG_WARN, and LOG_ERROR. It is assumed that these logging functions are properly defined and configured
+ *       in the system to provide meaningful logs for debugging and troubleshooting purposes.
  */
 void parser(buffer_t *buffer, const nodes_t *root_node);
 
